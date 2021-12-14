4330f6a10f72268ae5f1ca05d20b2d8d2f395cd7
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-12-14 08:09:56 +0200

    Initial commit

---
+++ src/main/java/com/example/mytranslate/MyTranslateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTranslateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTranslateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mytranslate/MyTranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytranslate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTranslateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
